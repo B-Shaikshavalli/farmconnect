c37a4a9cbf0ed89437e12afbc61ec43b2b426f6f
shaikshavalli B <bshaikshavalli143@gmail.com>
2025-03-20 21:50:25 +0530

    angular

--- angulr farm connect/src/app/auth.guard.ts
+++
@@ -1,8 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-import { FormconService } from './formcon.service';
-import { inject } from '@angular/core';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  
-  return  true;   
-};
